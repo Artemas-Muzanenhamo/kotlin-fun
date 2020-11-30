95329c18cc7ae8a9f929ea6a466132cb3939c83f
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-11-30 19:00:42 +0000

    add incomplete reduce example

---
+++ src/main/kotlin/com/functional/fun/reduce/ReduceExample.kt
@@ -0,0 +1,12 @@
+package com.functional.`fun`.reduce
+
+class ReduceExample {
+
+//        val strings = listOf("a", "b", "c", "d")
+//        println(strings.reduce { acc, string -> acc + string }) // abcd
+//        println(strings.reduceIndexed { index, acc, string -> acc + string + index }) // ab1c2d3
+//        println(strings.reduceRightIndexed { index, acc, string -> acc + string + index }) // abcd210
+    fun accumulateAllValues(letter: String): String {
+        TODO("IMPLEMENT THIS BRANDON! !!!!!!!! 😂")
+    }
+}
---
+++ src/test/kotlin/com/functional/fun/reduce/ReduceExampleTest.kt
@@ -0,0 +1,3 @@
+package com.functional.`fun`.reduce
+
+internal class ReduceExampleTest
